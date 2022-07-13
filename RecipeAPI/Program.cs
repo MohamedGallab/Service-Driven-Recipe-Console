a21c36900c85598f6590793a60527d097fca2bac
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-07-13 13:14:00 +0200

    removed swagger

--- RecipeAPI/Program.cs
+++ RecipeAPI/Program.cs
@@ -3,16 +3,7 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
