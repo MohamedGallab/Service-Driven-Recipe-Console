f01fc65bd1e932248445ded4b2e170abfab00031
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-08-01 20:33:22 +0200

    added swagger

--- RecipeAPI/Program.cs
+++ RecipeAPI/Program.cs
@@ -12,7 +12,17 @@ builder.Services.AddCors(options =>
 		});
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+	options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+	options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseCors("Cors Policy");
